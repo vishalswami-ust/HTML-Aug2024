dfa6678b1469942b662cec8e99c31dcb8c390c15
Vishal Swami(UST <284724@ust.com>
2024-09-05 11:00:37 +0530

    second

---
+++ typescrp/sandbox.js
@@ -0,0 +1,20 @@
+//Classes
+// let firstname="Vishal";
+// firstname = "Swami";
+// console.log(firstname);
+// let loc = "TRV";
+// console.log(loc);
+// let b = true;
+// console.log(b);
+// let c = 99.99;
+// console.log(c);
+// let aa = ["vishal", 99 , "Swami" , 7];
+// console.log(aa)
+// let bb:any;
+// bb= "String String String"
+// console.log(bb)
+// bb= 71001
+// console.log(bb)
+// let score = [["Rohit", 264, 54 , 130.43, 13000, 'India'], ["Virat", 183, 54 , 90, 18000, 'India']];
+// let total_run = score.reduce((accum, curVal)=> accum + curVal[4], 0 )
+// console.log(total_run) 
